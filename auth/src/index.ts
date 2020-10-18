8f8c03ae41a613b31d7b3cf3a7a5345b868a7467
gbryant-dev <gbryant1@hotmail.co.uk>
2020-10-18 19:22:58 +0100

    Add mongo to kubernetes cluster for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
+
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
@@ -25,6 +28,21 @@ app.use(errorHandler);
 
 const port = 3000;
 
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+      console.error(err);
+  }
+}
+
 app.listen(3000, () => {
-    console.log(`Listening on port ${port}!`);
+    console.log(`Listening on port ${port}`);
 });
+
+start();
