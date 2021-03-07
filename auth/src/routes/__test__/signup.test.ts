bb108d44facdd0fa56a3ad51b4ad64c7eb1302e0
gbryant-dev <gbryant1@hotmail.co.uk>
2021-03-07 15:09:56 +0000

    More auth tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -21,3 +21,40 @@ it('returns a 400 with an invalid email', async () => {
   })
   .expect(400);
 });
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'test',
+    password: 'f'
+  })
+  .expect(400);
+});
+
+
+it('returns a 400 with missing email and password', async () => {
+  return request(app)
+  .post('/api/users/signup')
+  .send({})
+  .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'test@test.com',
+    password: 'password'
+  })
+  .expect(201);
+
+  await request(app)
+  .post('/api/users/signup')
+  .send({
+    email: 'test@test.com',
+    password: 'password'
+  })
+  .expect(400);
+
+})
